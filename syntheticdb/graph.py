9c61a0e6349bab45c4218e433440208b85e6f200
Sam Corzine <samcorzine@Sams-MBP.AN-700-AP-I-AC>
2021-08-28 12:42:16 -0500

    refactor add failing test

--- syntheticdb/graph.py
+++ syntheticdb/graph.py
@@ -1,6 +1,6 @@
 import functools
 from dataclasses import dataclass
-from typing import List, Type, Callable, Union, TypeVar, Dict, Tuple, Iterable
+from typing import List, Type, Callable, Union, TypeVar, Dict, Tuple, Iterable, NewType
 from abc import ABC, abstractmethod
 
 import pandas
@@ -58,69 +58,122 @@ class TableView:
     sample: Callable[[], pd.DataFrame]
 
 
-class Table:
-    def __init__(
-        self,
-        name: str,
-        row_count: int,
-        pk_column: PrimaryKeyColumn,
-        fk_columns: List[ForeignKeyColumn],
-        dist_columns: List[DistributionColumn],
-    ):
-        self.name = name
-        self.row_count = row_count
-        self.pk_column = pk_column
-        self.fk_columns = fk_columns
-        self.dist_columns = dist_columns
+#
+#
+# class Table:
+#     def __init__(
+#         self,
+#         name: str,
+#         row_count: int,
+#         pk_column: PrimaryKeyColumn,
+#         fk_columns: List[ForeignKeyColumn],
+#         dist_columns: List[DistributionColumn],
+#     ):
+#         self.name = name
+#         self.row_count = row_count
+#         self.pk_column = pk_column
+#         self.fk_columns = fk_columns
+#         self.dist_columns = dist_columns
 
-    def sample(self) -> Iterable[pd.DataFrame]:
-        pass
-        # pk = count(1)
-        # for fk in self.fk_columns:
-        #
-        # vals = {col.name: col.sample() for col in self.dist_columns if isinstance(col, DistributionColumn)}
-        # return pd.DataFrame(vals)
+# @dataclass
+# class Database:
+#     tables: Dict[str, Table]
+#
+#
+#     def sample_from(self, table_name: str) -> pd.DataFrame:
+#         table = self.tables.get(table_name)
+#         if table is None:
+#             raise Exception("Invalid Table Name")
+#         col_iters = []
+#         col_iters.append((table.pk_column.name, (i for i in range(1, table.row_count + 1))))
+#         for fk in table.fk_columns:
+#             foreign_table = self.tables.get(fk.other_table)
+#             fk_iter = cycle(range(1, foreign_table.row_count))
+#             col_iters.append((fk.name, fk_iter))
+#         col_iters += [(col.name, (col.sample() for _ in count())) for col in table.dist_columns]
+#         for row in range(table.row_count):
+#             out_dict = {key: [next(val_iter)] for key, val_iter in col_iters}
+#             df = pd.DataFrame(out_dict)
+#             yield df
+#
+#     def select_star(self, table_name: str):
+#         dfs = []
+#         for df in self.sample_from(table_name):
+#             dfs.append(df)
+#         return pandas.concat(dfs)
+#
+#
+#
+#
+#
+# pk1 = PrimaryKeyColumn("test_pk_1")
+# val_key_1 = DistributionColumn("test_val_key", distribution=Uniform(0, 10))
+# t1 = Table("test_table_1",row_count=100, pk_column=pk1, fk_columns=[],  dist_columns=[val_key_1])
+# pk2 = PrimaryKeyColumn("test_pk_2")
+# val_key_2 = DistributionColumn("test_val_key_2", distribution=Uniform(5, 10))
+# fk_2 = ForeignKeyColumn("test_fk", other_table="test_table_1", other_column="test_pk_1")
+# t2 = Table("test_table_2", row_count=100, pk_column=pk2, fk_columns=[fk_2], dist_columns=[val_key_2])
+# db = Database({"test_table_1": t1, "test_table_2" : t2})
+# print(db.select_star("test_table_2"))
+#
+
+#
+#
+# def join(t1: Table, t2: Table, join_cols: Tuple[str, str]):
+#     table_col_map = {t.name: t.columns for t in [t1, t2]}
+#     first_col = join_cols[0].split(".")
+#     second_col = join_cols[1].split(".")
 
 
 @dataclass
-class Database:
-    tables: Dict[str, Table]
-
-    def sample_from(self, table_name: str) -> pd.DataFrame:
-        table = self.tables.get(table_name)
-        if table is None:
-            raise Exception("Invalid Table Name")
-        col_iters = []
-        col_iters.append((table.pk_column.name, (i for i in range(1, table.row_count + 1))))
-        for fk in table.fk_columns:
-            foreign_table = self.tables.get(fk.other_table)
-            fk_iter = cycle(range(1, foreign_table.row_count))
-            col_iters.append((fk.name, fk_iter))
-        col_iters += [(col.name, (col.sample() for _ in count())) for col in table.dist_columns]
-        for row in range(table.row_count):
-            out_dict = {key: [next(val_iter)] for key, val_iter in col_iters}
-            df = pd.DataFrame(out_dict)
-            yield df
-
-    def select_star(self, table_name: str):
-        dfs = []
-        for df in self.sample_from(table_name):
-            dfs.append(df)
-        return pandas.concat(dfs)
+class DistributionColumnNode:
+    name: str
 
 
+@dataclass
+class PrimaryKeyColumnNode:
+    name: str
 
 
+@dataclass
+class ForeignKeyColumnNode:
+    name: str
+    primary_key: PrimaryKeyColumnNode
 
-pk1 = PrimaryKeyColumn("test_pk_1")
-val_key_1 = DistributionColumn("test_val_key", distribution=Uniform(0, 10))
-t1 = Table("test_table_1",row_count=100, pk_column=pk1, fk_columns=[],  dist_columns=[val_key_1])
-pk2 = PrimaryKeyColumn("test_pk_2")
-val_key_2 = DistributionColumn("test_val_key_2", distribution=Uniform(5, 10))
-fk_2 = ForeignKeyColumn("test_fk", other_table="test_table_1", other_column="test_pk_1")
-t2 = Table("test_table_2", row_count=100, pk_column=pk2, fk_columns=[fk_2], dist_columns=[val_key_2])
-db = Database({"test_table_1": t1, "test_table_2" : t2})
-print(db.select_star("test_table_2"))
+
+@dataclass
+class TableNode:
+    name: str
+    columns: List[
+        Union[DistributionColumnNode, PrimaryKeyColumnNode, ForeignKeyColumnNode]
+    ]
+
+
+@dataclass
+class DBGraph:
+    nodes: List[
+        Union[
+            TableNode,
+            DistributionColumnNode,
+            PrimaryKeyColumnNode,
+            ForeignKeyColumnNode,
+        ]
+    ]
+
+
+def get_test_graph() -> DBGraph:
+    val_node_1 = DistributionColumnNode("num_employees")
+    pk_1 = PrimaryKeyColumnNode("company_id")
+    tbl_1 = TableNode("company", columns=[val_node_1, pk_1])
+    val_node_2 = DistributionColumnNode("income")
+    pk_2 = PrimaryKeyColumnNode("employee_id")
+    fk_2 = ForeignKeyColumnNode("company_id", primary_key=pk_1)
+    tbl_2 = TableNode("employee", columns=[val_node_2, pk_2, fk_2])
+    return DBGraph(nodes=[val_node_1, pk_1, tbl_1, val_node_2, pk_2, fk_2, tbl_2])
+
+
+graph = get_test_graph()
+
 
 @dataclass
 class SingleColDelimiter:
@@ -133,10 +186,54 @@ class DoubleColDelimiter:
     col: str
 
 
-def join(t1: Table, t2: Table, join_cols: Tuple[str, str]):
-    table_col_map = {t.name: t.columns for t in [t1, t2]}
-    first_col = join_cols[0].split(".")
-    second_col = join_cols[1].split(".")
+@dataclass
+class Select:
+    cols: List[DoubleColDelimiter]
 
 
-# col_group ->
+Table = NewType("Table", str)
+
+
+@dataclass
+class Join:
+    tables: Tuple[Table, Table]
+    # Assuming join on column equality
+    cols: Tuple[DoubleColDelimiter, DoubleColDelimiter]
+
+
+@dataclass
+class From:
+    table_view: Union[Table, Join]
+
+
+@dataclass
+class Query:
+    select_clause: Select
+    from_clause: From
+
+
+def get_test_query():
+    # SELECT employee.employee_id, employee.income, company.num_employees
+    #     FROM employee JOIN company ON employee.company_id = company.company_id
+    return Query(
+        select_clause=Select(
+            cols=[
+                DoubleColDelimiter(table="employee", col="employee_id"),
+                DoubleColDelimiter(table="employee", col="income"),
+                DoubleColDelimiter(table="company", col="num_employees"),
+            ]
+        ),
+        from_clause=From(
+            table_view=Join(
+                tables=(Table("employee"), Table("company")),
+                cols=(
+                    DoubleColDelimiter(table="employee", col="company_id"),
+                    DoubleColDelimiter(table="company", col="company_id"),
+                ),
+            )
+        ),
+    )
+
+
+def validate(db_graph: DBGraph, query: Query) -> bool:
+    pass
--- tests/test_sql.py
+++ tests/test_sql.py
@@ -1,57 +1,57 @@
-# content of test_sample.py
-from syntheticdb.query_parser import (
-    Query,
-    FloatRangeCondition,
-    WhereClause,
-    parse_sql_to_query,
-)
-
-
-def test_parse_table():
-    assert parse_sql_to_query("select * from synth_user") == Query(
-        table_name="synth_user", where_clauses=[], columns="*"
-    )
-
-
-def test_parse_where():
-    assert parse_sql_to_query("select * from synth_user where height < 5") == Query(
-        table_name="synth_user",
-        where_clauses=[WhereClause("height", condition=FloatRangeCondition(None, 5))],
-        columns="*",
-    )
-
-
-def test_parse_columns():
-    assert parse_sql_to_query(
-        "select age, height from synth_user where height < 5"
-    ) == Query(
-        table_name="synth_user",
-        where_clauses=[WhereClause("height", condition=FloatRangeCondition(None, 5))],
-        columns=["age", "height"],
-    )
-
-
-def test_db_join():
-    business_table = Table(
-        columns={
-            "business_id": PrimaryKeyColumn(),
-            "valuation": FloatColumn(Uniform(1_000_000, 1_000_000_000)),
-        },
-        row_count=10,
-    )
-    user_table = Table(
-        columns={
-            "user_id": PrimaryKeyColumn(),
-            "income": FloatColumn(Uniform(20_000, 80_000)),
-            "business_id": ForeignKeyColumn(
-                foreign_table="business", foreign_column="business_id"
-            ),
-        },
-        row_count=100,
-    )
-    db = DataBase(tables={"synth_user": user_table, "business": business_table})
-    df = db.select(
-        "select synth_user.income, business.valuation from synth_user "
-        + "join business on synthetic_user.business_id == business.valuation"
-    )
-    assert df.columns
+# # content of test_sample.py
+# from syntheticdb.query_parser import (
+#     Query,
+#     FloatRangeCondition,
+#     WhereClause,
+#     parse_sql_to_query,
+# )
+#
+#
+# def test_parse_table():
+#     assert parse_sql_to_query("select * from synth_user") == Query(
+#         table_name="synth_user", where_clauses=[], columns="*"
+#     )
+#
+#
+# def test_parse_where():
+#     assert parse_sql_to_query("select * from synth_user where height < 5") == Query(
+#         table_name="synth_user",
+#         where_clauses=[WhereClause("height", condition=FloatRangeCondition(None, 5))],
+#         columns="*",
+#     )
+#
+#
+# def test_parse_columns():
+#     assert parse_sql_to_query(
+#         "select age, height from synth_user where height < 5"
+#     ) == Query(
+#         table_name="synth_user",
+#         where_clauses=[WhereClause("height", condition=FloatRangeCondition(None, 5))],
+#         columns=["age", "height"],
+#     )
+#
+#
+# def test_db_join():
+#     business_table = Table(
+#         columns={
+#             "business_id": PrimaryKeyColumn(),
+#             "valuation": FloatColumn(Uniform(1_000_000, 1_000_000_000)),
+#         },
+#         row_count=10,
+#     )
+#     user_table = Table(
+#         columns={
+#             "user_id": PrimaryKeyColumn(),
+#             "income": FloatColumn(Uniform(20_000, 80_000)),
+#             "business_id": ForeignKeyColumn(
+#                 foreign_table="business", foreign_column="business_id"
+#             ),
+#         },
+#         row_count=100,
+#     )
+#     db = DataBase(tables={"synth_user": user_table, "business": business_table})
+#     df = db.select(
+#         "select synth_user.income, business.valuation from synth_user "
+#         + "join business on synthetic_user.business_id == business.valuation"
+#     )
+#     assert df.columns
